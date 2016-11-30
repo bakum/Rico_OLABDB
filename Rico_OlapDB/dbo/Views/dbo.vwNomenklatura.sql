4c2e0fdc3d4158034b4ffc0ced4cfd9c4ee84702
Bakum Viacheslav <bakum.v@gmail.com>
2016-11-30 23:00:24 +0200

    Currency upr added

--- Rico_OlapDB/dbo/Views/dbo.vwNomenklatura.sql
+++ Rico_OlapDB/dbo/Views/dbo.vwNomenklatura.sql
@@ -1,9 +1,10 @@
 ﻿
 
 
+
 CREATE VIEW [dbo].[vwNomenklatura]
 AS
-SELECT     convert(varchar(100), dbo.V8_Reference260.V8_ID, 1) AS ID, dbo.V8_Reference260.V8_Code, convert(varchar(2), dbo.V8_Reference260.V8_Fld641, 2) as Услуга, dbo.V8_Reference260.V8_Description, dbo.V8_Reference260.V8_Fld631 AS НаименованиеПолное, 
+SELECT     convert(varchar(100), dbo.V8_Reference260.V8_ID, 1) AS ID, dbo.V8_Reference260.V8_Code, convert(varchar(2), dbo.V8_Reference260.V8_Fld641, 2) as Услуга, (case when (convert(varchar(2), dbo.V8_Reference260.V8_Fld641, 2)) = '00' Then 'Товар' else 'Услуга' end) as Услуга_Имя, dbo.V8_Reference260.V8_Description, dbo.V8_Reference260.V8_Fld631 AS НаименованиеПолное, 
                       convert(varchar(2), dbo.V8_Reference260.V8_Marked, 2) AS Marked, convert(varchar(100), dbo.V8_Reference260.V8_Parent, 1) AS Parent, convert(varchar(2), dbo.V8_Reference260.V8_Folder, 2) AS Folder, dbo.V8_Reference260.V8_Fld616 AS Артикул, 
                       convert(varchar(100), dbo.V8_Reference260.V8_Fld617, 1) AS БазоваяЕдиницаИзмерения, dbo.V8_Reference197.V8_Description AS БазоваяЕдиницаИзмерения_Имя, 
                       convert(varchar(2), dbo.V8_Reference260.V8_Fld618, 2) AS Весовой, dbo.V8_Reference260.V8_Fld619 AS ВесовойКоэффициентВхождения, 
